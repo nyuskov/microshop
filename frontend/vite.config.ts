6a23f17ae0a7c765951014f2ff088734c0a50926
Nikita <nyuskov@gmail.com>
2025-09-28 12:46:32 +0300

    Add vite-plugin-checker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import checker from 'vite-plugin-checker'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +13,10 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    checker({
+      // e.g. use TypeScript check
+      typescript: true,
+    }),
   ],
   resolve: {
     alias: {
