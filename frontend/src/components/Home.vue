e395a73199248616cc9094e0347dd984cd388edd
Nikita <nyuskov@gmail.com>
2025-10-08 10:41:11 +0300

    Add flags to vue

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -5,47 +5,86 @@ import {
   backendServer
 } from '../stores/auth.ts';
 import Menubar from 'primevue/menubar';
-import { InputText, Badge } from 'primevue';
+import { InputText, Badge, Menu, Button } from 'primevue';
 import { ref, type Ref } from 'vue';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
+const redirectReg = "/auth/registration/";
+const redirectLogin = "/auth/login/";
 let isActiveUsers: Ref<boolean, boolean> = ref(false);
 let isActiveProducts: Ref<boolean, boolean> = ref(false);
+let isAuthorized: Ref<boolean, boolean> = ref(false);
+let popup: Ref<boolean, boolean> = ref(true);
 let saySomething: string = "Ебал я это ваше программирование!";
 console.log(saySomething);
 
 const items: Array<Object> = [
   {
-    label: 'Users',
+    label: 'Пользователи',
     icon: 'pi pi-fw pi-user',
-    command: () => { isActiveUsers.value = true; isActiveProducts.value = false; }
+    command: () => {
+      isActiveUsers.value = true;
+      isActiveProducts.value = false;
+      popup.value = !popup.value;
+    }
   },
   {
-    label: 'Products',
-    icon: 'pi pi-fw pi-user',
-    command: () => { isActiveUsers.value = false; isActiveProducts.value = true; }
+    label: 'Товары',
+    icon: 'pi pi-fw pi-cart-arrow-down',
+    command: () => {
+      isActiveUsers.value = false;
+      isActiveProducts.value = true;
+      popup.value = !popup.value;
+    }
+  },
+  {
+    separator: true,
+  },
+  {
+    label: 'Выйти',
+    icon: 'pi pi-fw pi-sign-out',
+    command: () => {
+      isActiveUsers.value = false;
+      isActiveProducts.value = false;
+      isAuthorized.value = false;
+      popup.value = !popup.value;
+    }
   },
 ];
+
+function toggle() {
+  popup.value = !popup.value;
+}
 
 </script>
 
 <template>
-  <Menubar :model="items">
-    <template #item="{ item, props, hasSubmenu, root }">
-      <a class="flex align-items-center" v-bind="props.action">
-        <span :class="item.icon" />
-        <span class="ml-2">{{ item.label }}</span>
-        <Badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge" />
-        <span v-if="item.shortcut" class="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">{{
-          item.shortcut }}</span>
-        <i v-if="hasSubmenu"
-          :class="['pi pi-angle-down', { 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root }]"></i>
-      </a>
-    </template>
-  </Menubar>
+  <div v-if="isAuthorized">
+    <Button type="button" icon="pi pi-ellipsis-v" @click="toggle" aria-haspopup="true" aria-controls="overlay_menu" />
+    <Menu ref="menu" id="overlay_menu" :model="items" :popup="popup">
+      <template #item="{ item, props }">
+        <a v-ripple class="flex align-items-center" v-bind="props.action">
+          <span :class="item.icon" />
+          <span class="ml-2">{{ item.label }}</span>
+          <Badge v-if="item.badge" class="ml-auto" :value="item.badge" />
+          <span v-if="item.shortcut" class="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">{{
+            item.shortcut }}</span>
+        </a>
+      </template>
+    </Menu>
+  </div>
   <div class="content">
     <h1>Hello!</h1>
-    <Users :isActiveUsers :backendServer></Users>
-    <Products :isActiveProducts :backendServer></Products>
+    <div v-if="!isAuthorized">
+      <Button severity="secondary" @click="router.push(redirectReg)">Зарегистрироваться</Button>
+      <span>или</span>
+      <Button severity="secondary" @click="router.push(redirectLogin)">Войти</Button>
+    </div>
+    <div v-if="isAuthorized">
+      <Users :isActiveUsers :backendServer></Users>
+      <Products :isActiveProducts :backendServer></Products>
+    </div>
   </div>
 </template>
 
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,10 +6,13 @@ import router from './router'
 
 import PrimeVue from 'primevue/config';
 import Theme from '@primevue/themes/nora';
+import { Ripple } from 'primevue';
+import 'primeicons/primeicons.css';
 
 
 const app = createApp(App)
 
+app.directive('ripple', Ripple)
 app.use(createPinia())
 app.use(router)
 
@@ -21,7 +24,8 @@ app.use(PrimeVue, {
             darkModeSelector: 'system', // Example darkModeSelector option
             cssLayer: false // Example cssLayer option
         }
-    }
+    },
+    ripple: true,
 });
 
 app.mount('#app')
